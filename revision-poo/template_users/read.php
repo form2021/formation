d6d9ea75bf84b54e8a79a5e5bb4fd5cf5197438a
LH <lh@applh.com>
2021-02-22 12:55:06 +0100

    ajout autre version

--- revision-poo/template_users/read.php
+++ revision-poo/template_users/read.php
@@ -1,9 +1,14 @@
 <?php
 // on mettra le read pour users
+// actuellement
+// pas de fonction
+// pas de classe
+// pas mvc
 
 // on commence à se connecter avec connect.php
 require_once "../connect.php";
 
+// model
 // on récupère les lignes
 // avec la requête
 $sql = "SELECT * FROM `users`";
@@ -13,7 +18,9 @@ $query = $db->query($sql);
 // on récupère les lignes sélectionnées
 $users = $query->fetchAll();        // tableau ordonné de tableaux associatifs
 
+// view => html
 ?>
+<!--
 <table>
     <thead>
         <tr>
@@ -32,6 +39,35 @@ $users = $query->fetchAll();        // tableau ordonné de tableaux associatifs
         <td><?= $user["sexe"] ?></td>
     </tr>
 <?php endforeach; ?>
+    </tbody>
+</table>
+-->
+
+<table>
+    <thead>
+        <tr>
+            <td>id</td>
+            <td>email</td>
+            <td>pseudo</td>
+            <td>sexe</td>
+        </tr>
+    </thead>
+    <tbody>
+<?php 
+foreach ($users as $user) {
+    // astuce du extract
+    extract($user); // => créé les variables $id, $email, $pseudo, $sexe
+    echo 
+    <<<x
+    <tr>
+        <td>$id</td>
+        <td>$email</td>
+        <td>$pseudo</td>
+        <td>$sexe</td>
+    </tr>
+    x;
+}
+?>    
     </tbody>
 </table>
 
